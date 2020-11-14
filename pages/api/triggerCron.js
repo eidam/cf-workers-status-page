0145fce7c402f2915f2fc7f5f32fa4afa562ec05
Adam Janis <adam.janis@kiwi.com>
2020-11-14 03:20:51 +0100

    chore(cleanup): comment manual cron trigger

--- pages/api/triggerCron.js
+++ pages/api/triggerCron.js
@@ -1,5 +1,6 @@
 import { processCronTrigger } from '../../src/functions/cronTrigger'
 
 export default async event => {
-  return processCronTrigger()
+  // used only for local debugging
+  //return processCronTrigger()
 }

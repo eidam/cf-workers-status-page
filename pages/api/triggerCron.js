c33beabef6084c2e3930fe05fb86391ca837e0ad
Adam Janis <adam.janis@kiwi.com>
2020-11-15 15:40:57 +0100

    feat(cron): detect and save location of last check

--- pages/api/triggerCron.js
+++ pages/api/triggerCron.js
@@ -2,5 +2,5 @@ import { processCronTrigger } from '../../src/functions/cronTrigger'
 
 export default async event => {
   // used only for local debugging
-  //return processCronTrigger()
+  //return processCronTrigger(event)
 }

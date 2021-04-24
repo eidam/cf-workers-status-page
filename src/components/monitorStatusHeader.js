0d4b584d282d63f66c8273e002c7ac0dcd3f5369
chiyi4488 <chiyi4869@gmail.com>
2021-04-24 21:59:55 +0800

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,9 +23,9 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
+            於{' '}
             {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
+            前檢查 (從{' '}
             {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
           </div>
         )}

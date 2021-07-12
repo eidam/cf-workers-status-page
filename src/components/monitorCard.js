a17c1dbb56ee759858f6bce9a3d782c8fc123d0c
Vilmantas Bieliūnas <vilmantasbieliunas@gmail.com>
2021-07-12 15:02:28 +0300

    Update monitorCard.js

--- src/components/monitorCard.js
+++ src/components/monitorCard.js
@@ -40,8 +40,8 @@ export default function MonitorCard({ key, monitor, data }) {
       <MonitorHistogram monitorId={monitor.id} kvMonitor={data} />
 
       <div className="flex flex-row justify-between items-center text-gray-400 text-sm">
-        <div>{config.settings.daysInHistogram} days ago</div>
-        <div>Today</div>
+        <div>{config.settings.daysInHistogram} dienos</div>
+        <div>Šiandien</div>
       </div>
     </div>
   )

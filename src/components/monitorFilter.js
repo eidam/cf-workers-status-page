8f63cc8450b84a132139f216abb47e9d7f669918
Vilmantas Bieliūnas <vilmantasbieliunas@gmail.com>
2021-07-12 15:03:44 +0300

    Update monitorFilter.js

--- src/components/monitorFilter.js
+++ src/components/monitorFilter.js
@@ -43,7 +43,7 @@ export default function MonitorFilter({ active, callback }) {
         value={input}
         onInput={handleInput}
         onKeyDown={handleKeyDown}
-        placeholder="Tap '/' to search"
+        placeholder="Spauskite '/' norint ieškoti"
         tabIndex={0}
         ref={(e) => e && active && e.focus()}
       />

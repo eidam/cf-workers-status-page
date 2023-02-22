15bf3f62cf6560c795846eb62b86b2b760b342ec
Miles Zimmerman <miles@zim.dev>
2023-02-21 20:57:27 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,26 +88,6 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
             />
           )
         })}
-        <div className="flex flex-row justify-between mt-4 text-sm">
-          <div>
-            Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
-            </a>
-            &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
-            </a>
-          </div>
-          <div>
-            <a
-              href="https://github.com/eidam/cf-workers-status-page"
-              target="_blank"
-            >
-              Get Your Status Page
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   )
